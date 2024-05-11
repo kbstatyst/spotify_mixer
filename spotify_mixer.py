57562281d69577db90c5eda10912e5bff7a4d044
Katarzyna Białoch <bialochkatarzyna@gmail.com>
2024-05-11 21:54:26 +0200

    Wbudowane playlisty i pokazywanie statystyk

--- spotify_mixer.py
+++ spotify_mixer.py
@@ -4,6 +4,7 @@ import base64
 import json
 import pandas
 import statistics
+import pandas as pd
 
 CLIENT_ID = ''
 CLIENT_SECRET = ''
@@ -14,12 +15,8 @@ API_URL = 'https://api.spotify.com/v1'
 PLAYLISTS_URL = API_URL + '/playlists'
 FEATURES_URL = API_URL + '/audio-features'
 
-LABELS = {'playlist_name': 'Nazwa playlisty:',
-          'playlist_owner': 'Właściciel playlisty',
-          'playlist_lenght': 'Liczba utworów'
-         }
-
-playlist_id = ''
+if 'playlist_id' not in st.session_state:
+    st.session_state['playlist_id'] = ''
 
 def get_access_token(client_id, client_secret):
     client_credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"
@@ -62,7 +59,6 @@ def get_audio_features(access_token, songs_ids):
         print("Failed to retrieve audio features.")
         return None
 
-
 def get_ids_from_playlist(playlist):
     ids = []
     for track in playlist['tracks']['items']:
@@ -70,70 +66,77 @@ def get_ids_from_playlist(playlist):
     return '%2C'.join(ids)
 
 def show_audio_features_info(audio_features):
-    st.caption('Średnie danceability')
-    st.text(statistics.mean(map((lambda t: t['danceability']), audio_features['audio_features'])))
-    st.caption('Średnie energy')
-    st.text(statistics.mean(map((lambda t: t['energy']), audio_features['audio_features'])))
-    st.caption('Średnie acousticness')
-    st.text(statistics.mean(map((lambda t: t['acousticness']), audio_features['audio_features'])))
-    st.caption('Średnie instrumentalness')
-    st.text(statistics.mean(map((lambda t: t['instrumentalness']), audio_features['audio_features'])))
-    st.caption('Średnie liveness')
-    st.text(statistics.mean(map((lambda t: t['liveness']), audio_features['audio_features'])))
-    st.caption('Średnie valence (szczęśliwość)')
-    st.text(statistics.mean(map((lambda t: t['valence']), audio_features['audio_features'])))
+    feature_names = {'taneczność': 'danceability',
+                     'energiczność': 'energy',
+                     'akustyczność': 'acousticness',
+                     'instrumentalność': 'instrumentalness',
+                     'żywość': 'liveness',
+                     'szczęśliwość': 'valence'}
 
-def show_playlist_info(playlist):
-    st.caption(LABELS['playlist_name'])
-    st.text(playlist['name'])
-    # st.caption(LABELS['playlist_owner'])
-    # st.text(playlist['owner']['display_name'])
-    st.caption(LABELS['playlist_lenght'])
-    st.text(len(playlist['tracks']['items']))
+    keys = list(feature_names.keys())
+    row1 = keys[0:3]
+    row2 = keys[3:6]
 
-pl_id = st.text_input("Input playlist id", value=playlist_id)
+    columns1 = st.columns(3)
+    columns2 = st.columns(3)
+
+    def mean(name):
+        return statistics.mean(map((lambda t: t[feature_names[name]]), audio_features['audio_features']))
+
+    for (name, column) in zip(row1, columns1):
+        column.metric(name, mean(name))
+
+    for (name, column) in zip(row2, columns2):
+        column.metric(name, mean(name))
+
+def songs_to_dataframe(songs):
+    # df = pd.DataFrame(np.random.randn(10, 5), columns=("col %d" % i for i in range(5)))
+    songs_info = []
+    for s in songs:
+        songs_info.append({'Artysta': s['track']['artists'][0]['name'], 'Tytuł': s['track']['name']})
+
+    df = pd.DataFrame(songs_info)
+    st.table(df)
+
+st.header("Spotify mixer")
+st.caption("Playlisty")
 
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    pl1 = st.button("Playlista wbudowana 1")
+    pl1 = st.button("Piosenki do płakania", use_container_width=True)
 
 with col2:
-    pl2 = st.button("Playlista wbudowana 2")
+    pl2 = st.button("Piosenki do tańczenia", use_container_width=True)
 
 with col3:
-    pl3 = st.button("Playlista wbudowana 3")
+    pl3 = st.button("Spokojny jazz", use_container_width=True)
 
 with col4:
-    pl4 = st.button("Playlista wbudowana 3")
+    pl4 = st.button("Szybki jogging", use_container_width=True)
 
 if pl1:
-    playlist_id = '' # playlista 1
+    st.session_state['playlist_id'] = '37i9dQZF1EIdChYeHNDfK5'
 elif pl2:
-    playlist_id = '' # playlista 2
+    st.session_state['playlist_id'] = '7qZcphQz8AJd7V4CmUI4dA'
 elif pl3:
-    playlist_id = '' # playlista 3
+    st.session_state['playlist_id'] = '6c8RcZLx8DGlPdq7XLVr9g'
 elif pl4:
-    playlist_id = '' # playlista 4
+    st.session_state['playlist_id'] = '0zFDgi3VsLZUHH9NerfQ8t'
 
 access_token = get_access_token(CLIENT_ID, CLIENT_SECRET)
 
-if access_token:
-    playlist = get_playlist(access_token, playlist_id)
+if access_token and st.session_state['playlist_id']:
+    playlist = get_playlist(access_token, st.session_state['playlist_id'])
     if playlist:
-        # songs = get_songs_from_playlist(playlist)
-        show_playlist_info(playlist)
-        for (n, i) in enumerate(playlist['tracks']['items']):
-            st.write(str(n + 1) + '. ' + i['track']['artists'][0]['name'] + ' : ' + i['track']['name'])
 
         stats = st.button('Statystyki')
         if stats:
             songs_ids = get_ids_from_playlist(playlist)
             audio_features = get_audio_features(access_token, songs_ids)
             show_audio_features_info(audio_features)
-        # st.write(audio_features)
 
-        # st.write(songs)
+        songs_to_dataframe(playlist['tracks']['items'])
     else:
         print("No playlists found.")
 
