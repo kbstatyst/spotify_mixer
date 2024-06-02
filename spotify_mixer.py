23d57f5686c281972908329d17af7de07945ba8d
Katarzyna Białoch <bialochkatarzyna@gmail.com>
2024-06-02 22:54:06 +0200

    Dodanie obrazków

--- spotify_mixer.py
+++ spotify_mixer.py
@@ -260,9 +260,7 @@ def new_playlist():
     def lengths(all, filtered):
         expander = st.expander('Liczba utworów', expanded=True)
         with expander:
-            columns = st.columns(2)
-            columns[0].metric('Wszyskie', all)
-            columns[1].metric('Spełniające warunki', filtered)
+            st.metric('Spełniające warunki', filtered)
 
     # zapisanie playlisty
     if btn:
@@ -306,16 +304,20 @@ def mainpage():
 
     # kolumny z przyciskami z wbudowanymi playlistami
     with col1:
-        pl1 = st.button(':minidisc: Piosenki do płakania', use_container_width=True)
+        col1.image('https://cdn-icons-png.flaticon.com/512/636/636224.png')
+        pl1 = st.button('Piosenki do płakania', use_container_width=True)
 
     with col2:
-        pl2 = st.button(":minidisc: Piosenki do tańczenia", use_container_width=True)
+        col2.image('https://cdn-icons-png.flaticon.com/512/636/636224.png')
+        pl2 = st.button("Piosenki do tańczenia", use_container_width=True)
 
     with col3:
-        pl3 = st.button(":minidisc: Spokojny jazz", use_container_width=True)
+        col3.image('https://cdn-icons-png.flaticon.com/512/636/636224.png')
+        pl3 = st.button("Spokojny jazz", use_container_width=True)
 
     with col4:
-        pl4 = st.button(":minidisc: Szybki jogging", use_container_width=True)
+        col4.image('https://cdn-icons-png.flaticon.com/512/636/636224.png')
+        pl4 = st.button("Szybki jogging", use_container_width=True)
 
     if pl1:
         st.session_state['playlist_id'] = '37i9dQZF1EIdChYeHNDfK5'
@@ -333,9 +335,14 @@ def mainpage():
         chunk = custom_playlists[i:i + 4]
         col1, col2, col3, col4 = st.columns(4)
         try:
-            col1.button(":dvd: " + chunk[0]['name'], key=uuid.uuid1(), use_container_width=True, on_click=show_custom_playlist, args=(chunk[0],))
+            col1.image('https://cdn-icons-png.flaticon.com/512/636/636224.png')
+            col1.button(chunk[0]['name'], key=uuid.uuid1(), use_container_width=True, on_click=show_custom_playlist, args=(chunk[0],))
+            col2.image('https://cdn-icons-png.flaticon.com/512/636/636224.png')
             col2.button(":dvd: " + chunk[1]['name'], key=uuid.uuid1(), use_container_width=True, on_click=show_custom_playlist, args=(chunk[1],))
+            col3.button(":dvd: " + chunk[1]['name'], key=uuid.uuid1(), use_container_width=True, on_click=show_custom_playlist, args=(chunk[1],))
             col3.button(":dvd: " + chunk[2]['name'], key=uuid.uuid1(), use_container_width=True, on_click=show_custom_playlist, args=(chunk[2],))
+            col3.button(":dvd: " + chunk[2]['name'], key=uuid.uuid1(), use_container_width=True, on_click=show_custom_playlist, args=(chunk[2],))
+            col4.button(":dvd: " + chunk[1]['name'], key=uuid.uuid1(), use_container_width=True, on_click=show_custom_playlist, args=(chunk[1],))
             col4.button(":dvd: " + chunk[3]['name'], key=uuid.uuid1(), use_container_width=True, on_click=show_custom_playlist, args=(chunk[3],))
         except IndexError:
             break
